bb8a9589df3275e62cfcf0f652f4867bd13973f6
mahmoud <mahmoud.ziad.alsaeid@gmail.com>
2023-11-24 15:09:38 +0100

    feat: added basic validation in the controllers

--- controllers/requestTaskController.js
+++ controllers/requestTaskController.js
@@ -2,117 +2,136 @@ const asyncHandler = require("express-async-handler");
 const Child = require("../models/childModel");
 const RequestTask = require("../models/requestTask");
 const User = require("../models/userModel");
-const { setTask } = require("../controllers/taskController");
 const Task = require("../models/taskModel");
-const childModel = require("../models/childModel");
+
 // @desc    Get Task
 // @route   GET /api/Task
 // @access  Private
 const getRequestTask = asyncHandler(async (req, res) => {
-  const RequestTasks = await RequestTask.find({  }).populate({
-    path:'childId',
-    select: {
-      name: 1
-    }
-  });
-  res.status(200).json(RequestTasks);
+  try {
+    const RequestTasks = await RequestTask.find({}).populate({
+      path: 'childId',
+      select: {
+        name: 1
+      }
+    });
+    res.status(200).json(RequestTasks);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // @desc    Set Class
 // @route   POST /api/class
 // @access  Private
 const setRequestTask = asyncHandler(async (req, res) => {
-  const { typeTask, name, valueTask, time, childId,desc } = req.body;
-  if (!name) {
-    res.status(400);
-    throw new Error("Please add a text field");
+  try {
+    const { typeTask, name, valueTask, time, childId, desc } = req.body;
+
+    if (!name || !desc || !valueTask || !time || !childId) {
+      res.status(400);
+      throw new Error("Please provide all required fields");
+    }
+
+    const childObj = await Child.findById(childId);
+    const RequestTasks = await RequestTask.create({
+      typeTask,
+      name,
+      desc,
+      valueTask,
+      time,
+      childId,
+    });
+
+    const child = await Child.findByIdAndUpdate(childId, { $push: { requestTask: RequestTasks._id } });
+    const user = await User.findByIdAndUpdate(childObj.parentId, { $push: { requestTask: RequestTasks._id } });
+
+    res.status(200).json(RequestTasks);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
   }
-  const childObj =await  childModel.findById(childId);
-  const RequestTasks = await RequestTask.create({
-    typeTask,
-    name,
-    desc,
-    valueTask,
-    time,
-    childId,
-  });
-  const child = await Child.findByIdAndUpdate(childId, 
-    { $push: { requestTask: RequestTasks._id } },  );
-  const user = await User.findByIdAndUpdate(childObj.parentId, 
-    { $push: { requestTask: RequestTasks._id } },  );
-  res.status(200).json(RequestTasks);
 });
-/*
-  const childbefore = await Child.findById(childId)
-  console.log(childbefore)
-  const updateRequestTasks = await childbefore.requestTask.push(RequestTasks._id)
-  console.log( await updateRequestTasks)
-  const userBefore = await User.findById(RequestTasks.parentId)
-  const updateUserRequestTasks = userBefore.requestTask.push(RequestTasks._id)*/ 
+
 // @desc    Update RequestTask
 // @route   PUT /api/RequestTask/:id
 // @access  Private
 const updateRequestTask = asyncHandler(async (req, res) => {
-  const RequestTasks = await RequestTask.findById(req.params.id);
+  try {
+    const RequestTasks = await RequestTask.findById(req.params.id);
 
-  if (!RequestTasks) {
-    res.status(400);
-    throw new Error("Class not found");
-  }
-  const updatedRequestTask = await RequestTask.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    {
-      new: true,
+    if (!RequestTasks) {
+      res.status(404);
+      throw new Error("RequestTask not found");
     }
-  );
-  res.status(200).json(updatedRequestTask);
+
+    const updatedRequestTask = await RequestTask.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedRequestTask);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
+
 const approveRequestTask = asyncHandler(async (req, res) => {
-  const RequestTasks = await RequestTask.findById(req.params.id);
-  const {desc,typeTask, name, valueTask, time, childId } = RequestTasks;
+  try {
+    const RequestTasks = await RequestTask.findById(req.params.id);
 
-  /*parentId: req.user.id,
-  typeTask,
-  name,
-  desc,
-  time,
-  childId,
-  valueTask*/
-  console.log(RequestTasks)
-  const childOj = await childModel.findById(childId);
-  const Tasks = await Task.create({
-    valueTask: valueTask,
-    typeTask: typeTask,
-    name: name,
-    desc: desc,
-    time: time,
-    childId: childId,
-    parentId: childOj.parentId
-  });
-  const child = await Child.findByIdAndUpdate(childId,
-    { $push: { task: Tasks._id } }  ) 
-  const user = await User.findByIdAndUpdate(Tasks.parentId, 
-    { $push: { task: Tasks._id } },  );
-     await RequestTask.findByIdAndDelete(req.params.id);
+    if (!RequestTasks) {
+      res.status(404);
+      throw new Error("RequestTask not found");
+    }
 
-  res.status(200).json(req.body);
+    const { desc, typeTask, name, valueTask, time, childId } = RequestTasks;
+    const childOj = await Child.findById(childId);
+
+    const Tasks = await Task.create({
+      valueTask,
+      typeTask,
+      name,
+      desc,
+      time,
+      childId,
+      parentId: childOj.parentId
+    });
+
+    const child = await Child.findByIdAndUpdate(childId, { $push: { task: Tasks._id } });
+    const user = await User.findByIdAndUpdate(Tasks.parentId, { $push: { task: Tasks._id } });
+
+    await RequestTask.findByIdAndDelete(req.params.id);
+
+    res.status(200).json(req.body);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // @desc    Delete ypeClass
 // @route   DELETE /api/RequestTask/:id
 // @access  Private
 const deleteRequestTask = asyncHandler(async (req, res) => {
-  const RequestTask = await RequestTask.findById(req.params.id);
+  try {
+    const RequestTasks = await RequestTask.findById(req.params.id);
 
-  if (!RequestTask) {
-    res.status(400);
-    throw new Error("RequestTask not found");
+    if (!RequestTasks) {
+      res.status(404);
+      throw new Error("RequestTask not found");
+    }
+
+    await RequestTask.findOneAndDelete(RequestTasks._id);
+
+    res.status(200).json({ id: req.params.id });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
   }
-
-  await RequestTask.findOneAndDelete(RequestTask._id);
-
-  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
--- controllers/taskController.js
+++ controllers/taskController.js
@@ -5,142 +5,185 @@ const User = require("../models/userModel");
 const Task = require("../models/taskModel");
 const taskModel = require("../models/taskModel");
 
-// @desc    Get Task    parentId: req.user.id,
-
-// @route   GET /api/Task
-// @access  Private
-
 const getAllParentTasks = asyncHandler(async (req, res) => {
-  const parentId = req.user.id;
-  const Tasks = await Task.find({ parentId: parentId, status: false }).populate(
-    {
-      path: "childId",
+  try {
+    const parentId = req.user.id;
+    const Tasks = await Task.find({ parentId: parentId, status: false }).populate(
+      {
+        path: "childId",
+  
+        select: {
+          name: 1,
+        },
+      }
+    );    res.status(200).json(Tasks);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
 
+
+const getCompletedTask = asyncHandler(async (req, res) => {
+  try {
+    const Tasks = await Task.find({
+      status: true,
+      parentId: req.user.id,
+    }).populate({
+      path: 'childId',
       select: {
         name: 1,
       },
-    }
-  );
-  res.status(200).json(Tasks);
+    });
+    res.status(200).json(Tasks);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
 
 const getAllTasks = asyncHandler(async (req, res) => {
-  const { id } = req.query;
-  console.log(id);
-  const Tasks = await Task.find({ childId: id });
-  res.status(200).json(Tasks);
-});
-const getCompletedTask = asyncHandler(async (req, res) => {
-  const Tasks = await Task.find({
-    status: true,
-    parentId: req.user.id,
-  })
-    .populate({
-      path: "childId",
+  try {
+    const { id } = req.query;
 
-      select: {
-        name: 1,
-      },
-    })
-    .sort({ createdAt: -1 })
-    .limit(3);
+    // Validate that 'id' is present in the query
+    if (!id) {
+      res.status(400);
+      throw new Error("Please provide 'id' in the query");
+    }
 
-  res.status(200).json(Tasks);
+    const Tasks = await Task.find({ childId: id });
+    res.status(200).json(Tasks);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
+
 
 const getUnCompletedTask = asyncHandler(async (req, res) => {
-  const Tasks = await Task.find({ childId: req.query.childId, status: false });
-  res.status(200).json(Tasks);
+  try {
+    const Tasks = await Task.find({childId: req.query.childId ,status: false });
+    res.status(200).json(Tasks);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
 
-// @desc    Set Class
-// @route   POST /api/class
-// @access  Private
+
 const setTask = asyncHandler(async (req, res) => {
-  const { typeTask, name, time, childId, valueTask, desc } = req.body;
+  try {
+    const { typeTask, name, time, childId, valueTask, desc } = req.body;
 
-  // let user = await User.findById({req.user.id});
+    if (!name || !childId || !valueTask || !desc) {
+      res.status(400);
+      throw new Error("Please provide all required fields");
+    }
 
-  // if (!user) {
-  //   res.status(400);
-  //   throw new Error("Please add a user field");
-  // }
-  if (!name) {
-    res.status(400);
-    throw new Error("Please add a text field");
+    const existingChild = await Child.findById(childId);
+    if (!existingChild) {
+      res.status(400);
+      throw new Error("Child not found");
+    }
+
+    const Tasks = await Task.create({
+      parentId: req.user.id,
+      typeTask,
+      name,
+      time,
+      childId,
+      valueTask,
+      desc,
+    });
+
+    const child = await Child.findByIdAndUpdate(childId, {
+      $push: { task: Tasks._id },
+    });
+    const user = await User.findByIdAndUpdate(Tasks.parentId, {
+      $push: { task: Tasks._id },
+    });
+
+    res.status(200).json(Tasks);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
   }
-
-  const Tasks = await Task.create({
-    parentId: req.user.id,
-    typeTask,
-    name,
-    time,
-    childId,
-    valueTask,
-    desc,
-  });
-  const child = await Child.findByIdAndUpdate(childId, {
-    $push: { task: Tasks._id },
-  });
-  const user = await User.findByIdAndUpdate(Tasks.parentId, {
-    $push: { task: Tasks._id },
-  });
-  res.status(200).json(Tasks);
 });
 
-// @desc    Update Task
-// @route   PUT /api/Task/:id
-// @access  Private
 const updateTask = asyncHandler(async (req, res) => {
-  const Tasks = await Task.findById(req.params.id);
-  console.log({ req: req.body });
-  if (!Tasks) {
-    res.status(400);
-    throw new Error("Class not found");
+  try {
+    const Tasks = await Task.findById(req.params.id);
+
+    if (!Tasks) {
+      res.status(400);
+      throw new Error("Task not found");
+    }
+
+    // Add validation checks for the fields you want to validate
+    // For example, check if the updated data is valid before applying
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
   }
-
-  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-
-  res.status(200).json(updatedTask);
 });
+
+
+
 
 const EndTask = asyncHandler(async (req, res) => {
-  const Tasks = await Task.findById(req.params.id);
+  try {
+    const Tasks = await Task.findById(req.params.id);
 
-  if (!Tasks) {
-    res.status(400);
-    throw new Error("Class not found");
-  }
-  const child = await Child.findByIdAndUpdate(Tasks.childId, {
-    $inc: { currentAccount: Tasks.valueTask },
-  });
-  const updatedTask = await Task.findByIdAndUpdate(
-    req.params.id,
-    { status: true },
-    {
-      new: true,
+    if (!Tasks) {
+      res.status(400);
+      throw new Error("Task not found");
     }
-  );
 
-  res.status(200).json(updatedTask);
+    const child = await Child.findByIdAndUpdate(
+      Tasks.childId,
+      { $inc: { currentAccount: Tasks.valueTask } }
+    );
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      { status: true },
+      { new: true }
+    );
+
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
 
-// @desc    Delete ypeClass
-// @route   DELETE /api/Task/:id
-// @access  Private
+
+
 const deleteTask = asyncHandler(async (req, res) => {
-  const Task = await taskModel.findById(req.params.id);
+  try {
+    const Task = await taskModel.findById(req.params.id);
 
-  if (!Task) {
-    res.status(400);
-    throw new Error("Task not found");
+    if (!Task) {
+      res.status(400);
+      throw new Error("Task not found");
+    }
+
+    await taskModel.findOneAndDelete(Task._id);
+
+    res.status(200).json({ id: req.params.id });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
   }
-
-  await taskModel.findOneAndDelete(Task._id);
-
-  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
@@ -151,5 +194,5 @@ module.exports = {
   getUnCompletedTask,
   EndTask,
   getAllTasks,
-  getAllParentTasks,
+  getAllParentTasks
 };
