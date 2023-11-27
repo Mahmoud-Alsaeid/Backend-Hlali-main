69fff081e7acb254eed8ae30c8032e158ebd9076
abdalsalam_albirawi <abdalsalamalbirawi@gmail.com>
2023-11-27 10:42:00 -0800

    last commit from abo albaraa

--- routes/TransactionRoutes.js
+++ routes/TransactionRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const {
   getTransaction,
   setTransaction,
-  internalTranaction,
+  internalTransaction,
   fromFather,
 } = require("../controllers/transactionController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(getTransaction).post(setTransaction);
 router.route("/fromFather/:id").post(fromFather);
-router.route("/:id").post(internalTranaction);
+router.route("/:id").post(internalTransaction);
 
 module.exports = router;
